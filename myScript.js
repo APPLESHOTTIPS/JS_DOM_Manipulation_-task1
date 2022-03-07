c7a65178dc4115b34c10c109639b20d63ea94ce4
aravind <arvindchowdary60@gmail.com>
2022-03-07 23:18:48 +0530

    javascript

---
+++ myScript.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+
+var x = {
+    type: "pita",
+    model: "500",
+    color: "black"
+
+};
+x.color = "red";
+x.woner = "arvind";
+x.tyres = "MRF";
+document.getElementById("demo").innerHTML = "car owner is " + '' + x.tyres;
